c3bb8f3c8964606124c37ab661f6ced1ac2a9d8d
Jose Ramon Mañes <jr.mb47@gmail.com>
2021-03-04 13:12:39 +0100

    add golang library and export to metrics path

--- golang-exporter/app/main.go
+++ golang-exporter/app/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"flag"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -16,18 +17,18 @@ var addr = flag.String("listen-address", ":8081", "The address to listen on for 
 
 var (
 	c = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "codely_app_sample_metric",
-		Help: "Sample counter for Codely course",
+		Name: "jrmanes_app_sample_metric",
+		Help: "Sample counter for jrmanes course",
 	})
 
 	h = promauto.NewHistogram(prometheus.HistogramOpts{
-		Name: "codely_app_sample_histogram",
-		Help: "Sample histogram for Codely course",
+		Name: "jrmanes_app_sample_histogram",
+		Help: "Sample histogram for jrmanes course",
 	})
 
 	d = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "codely_app_sample_devices",
-		Help: "Sample counter with devices label for Codely course"}, []string{"device"})
+		Name: "jrmanes_app_sample_devices",
+		Help: "Sample counter with devices label for jrmanes course"}, []string{"device"})
 )
 
 func main() {
@@ -38,7 +39,7 @@ func main() {
 		for {
 			rand.Seed(time.Now().UnixNano())
 			h.Observe(float64(rand.Intn(100-0+1) + 0))
-			d.With(prometheus.Labels{"device":"/dev/sda"}).Inc()
+			d.With(prometheus.Labels{"device": "/dev/sda"}).Inc()
 			c.Inc()
 			time.Sleep(1 * time.Second)
 		}
